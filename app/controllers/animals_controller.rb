dc7510e0967028c7e55460db0e2533730aa6a305
Colton Davis <colt822lavay@outlook.com>
2018-11-20 08:57:41 -0700

    Fix alignment, add styles, debug issues

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,5 +1,5 @@
 class AnimalsController < ApplicationController
-  before_action :set_animal, only: [:show, :edit, :update]
+  before_action :set_animal, only: [:show, :edit, :update, :destroy]
 
   def index
     @animals = current_zoo.animals 
